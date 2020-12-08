554198766df9068963d737907a2c8689362a9cc6
hws522 <sraphic@kakao.com>
2020-12-08 23:22:22 +0900

    pagination

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-saramin_result = requests.get("http://www.saramin.co.kr/zf_user/search/recruit?search_area=main&search_done=y&search_optional_item=n&searchType=default_popular&searchword=python&recruitPage=2&recruitSort=relation&recruitPageCount=50&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&quick_apply=&except_read=")
+saramin_result = requests.get("http://www.saramin.co.kr/zf_user/search/recruit?search_area=main&search_done=y&search_optional_item=n&searchType=default_popular&searchword=python&recruitPage=1&recruitSort=relation&recruitPageCount=50&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&quick_apply=&except_read=")
 
 saramin_soup = BeautifulSoup(saramin_result.text, "html.parser")
 
@@ -11,8 +11,12 @@ links = pagination.find_all('a')
 
 pages = []
 
-for link in links:
-    pages.append(link.find("span"))
+for link in links[:-1]:
+    pages.append(int(link.string))
 
-print(pages[:-1])
 #links를 가져온다음 빈 array를 만들고, links 안의 link 마다 span을 찾아서 pages 에 넣어줌.
+#pages에 페이지 숫자만 넘겨주기 위해 문자열을 숫자로 바꾸어 삽입.
+
+max_page = pages[-1]
+# 마지막페이지 
+
