a653e0096b6be1f15e0bf0bde86f03ec74e196cd
hws522 <sraphic@kakao.com>
2020-12-10 02:35:21 +0900

    def get_job add

--- saramin.py
+++ saramin.py
@@ -2,10 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 LIMIT = 50
-SARAMIN_URL = f"http://www.saramin.co.kr/zf_user/search/recruit?search_area=main&search_done=y&search_optional_item=n&searchType=default_popular&searchword=python&recruitPage=1&recruitSort=relation&recruitPageCount={LIMIT}&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&quick_apply=&except_read="
+search_word_python = "python"
+SARAMIN_URL_PY = f"http://www.saramin.co.kr/zf_user/search/recruit?search_area=main&search_done=y&search_optional_item=n&searchType=default_popular&searchword={search_word_python}&recruitPage=1&recruitSort=relation&recruitPageCount={LIMIT}&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&quick_apply=&except_read="
 
 def extract_saramin_pages():
-    result = requests.get(SARAMIN_URL)
+    result = requests.get(SARAMIN_URL_PY)
 
     soup = BeautifulSoup(result.text, "html.parser")
 
@@ -31,17 +32,22 @@ def extract_job(html):
     location = html.find("div", {"class": "job_condition"}).find("a").string
     job_id = html["value"]
     
-    return {'title': title, 'company': company, 'location': location, 'link': f"http://www.saramin.co.kr/zf_user/jobs/relay/view?isMypage=no&rec_idx={job_id}&recommend_ids=eJxVkLEVxDAIQ6e5HjAgqDOI99%2FinPglhlL%2BQhaMMB8JTEv%2F4RpFTr0fUpTTZYLGlgRhW9xe6kqHfvKbXm5tPKPK0ENJkIyajbRCOZwKFeYsUa88vQPWvtpy81h5nm2RUPRmKPbIdRKtdkPdS8j7tFnrllwp4jH%2FAX0HU3I%3D&view_type=search&searchword=python&searchType=default_popular&gz=1&t_ref_content=generic&t_ref=search&paid_fl=n#seq=0"}
+    return {'title': title, 'company': company, 'location': location, 'link': f"http://www.saramin.co.kr/zf_user/jobs/relay/view?isMypage=no&rec_idx={job_id}&recommend_ids=eJxVkLEVxDAIQ6e5HjAgqDOI99%2FinPglhlL%2BQhaMMB8JTEv%2F4RpFTr0fUpTTZYLGlgRhW9xe6kqHfvKbXm5tPKPK0ENJkIyajbRCOZwKFeYsUa88vQPWvtpy81h5nm2RUPRmKPbIdRKtdkPdS8j7tFnrllwp4jH%2FAX0HU3I%3D&view_type=search&searchword={search_word_python}&searchType=default_popular&gz=1&t_ref_content=generic&t_ref=search&paid_fl=n#seq=0"}
 
 
 def extract_saramin_jobs(last_page):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping page {page}")
-        result = requests.get(f"{SARAMIN_URL}&recruitPageCount={page*LIMIT}")
+        result = requests.get(f"{SARAMIN_URL_PY}&recruitPageCount={page*LIMIT}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "item_recruit"})
         for result in results:
             job = extract_job(result)
             jobs.append(job)
     return jobs
+
+def get_jobs():
+    last_saramin_page = extract_saramin_pages()
+    saramin_jobs = extract_saramin_jobs(last_saramin_page)
+    return saramin_jobs
