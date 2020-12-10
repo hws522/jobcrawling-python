3ab36f56ec2db2e8cd4e27e6b2123fa2d417a743
hws522 <sraphic@kakao.com>
2020-12-11 05:58:12 +0900

    style alter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from saramin import get_jobs
-from jobkorea import get_jobs 
+from saramin import get_jobs as get_saramin_jobs
+from jobkorea import get_jobs as get_jobkorea_jobs
 
-#print(get_jobs()) 
-print(get_jobs())
+saramin_jobs = get_saramin_jobs()
+jobkorea_jobs = get_jobkorea_jobs()
+
+# print(saramin_jobs)
+print(jobkorea_jobs)
