5674dc9d88f929d9c5d37b8cc257d14c361b7ce1
hws522 <sraphic@kakao.com>
2020-12-11 10:06:30 +0900

    jobkorea title add

--- jobkorea.py
+++ jobkorea.py
@@ -2,32 +2,42 @@ import requests
 from bs4 import BeautifulSoup
 
 search_word_python = "python"
-JOBKOREA_URL_PY = f"http://www.jobkorea.co.kr/Search/?stext={search_word_python}&tabType=recruit"
+JOBKOREA_URL_PY = (
+    f"http://www.jobkorea.co.kr/Search/?stext={search_word_python}&tabType=recruit"
+)
+
 
 def get_last_page():
     result = requests.get(JOBKOREA_URL_PY)
 
     soup = BeautifulSoup(result.text, "html.parser")
 
-    pagination = soup.find("div", {"class":"tplPagination"}).find_all("li")
-# 찾은 결과를 pagination 에 넣어줘서 리스트를 만든뒤에 pages 변수에 넣어줌.
+    pagination = soup.find("div", {"class": "tplPagination"}).find_all("li")
+    # 찾은 결과를 pagination 에 넣어줘서 리스트를 만든뒤에 pages 변수에 넣어줌.
     last_page = pagination[-1].text
     return int(last_page)
+
+
+def extract_job(html):
+    title = html.find("div", {"class": "post-list-info"}).find("a")["title"]
+
+    print(title)
+
 
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
         result = requests.get(f"{JOBKOREA_URL_PY}&Page_No={page+1}")
         soup = BeautifulSoup(result.text, "html.parser")
-        results = soup.find_all("li", {"class":"list-post"})
+        soup = soup.find("div", {"class": "recruit-info"})
+        results = soup.find_all("li", {"class": "list-post"})
         for result in results:
-            print(result["data-gno"])
-            if result is None:
-                continue
-            
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
+
 
 def get_jobs():
     last_page = get_last_page()
     jobs = extract_jobs(last_page)
-    return jobs
-    
+    return last_page
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from saramin import get_jobs as get_saramin_jobs
+# from saramin import get_jobs as get_saramin_jobs
+
 from jobkorea import get_jobs as get_jobkorea_jobs
 
-saramin_jobs = get_saramin_jobs()
+# saramin_jobs = get_saramin_jobs()
 jobkorea_jobs = get_jobkorea_jobs()
 
 # print(saramin_jobs)
