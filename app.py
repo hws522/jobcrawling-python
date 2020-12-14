e93edd4d9d5cb4bcedf5bf73b5f84a2a9f05a9ee
hws522 <sraphic@kakao.com>
2020-12-15 04:29:06 +0900

    try except add

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from saramin import get_jobs
-
+from save import save_to_file
 
 app = Flask("jobScrapper")
 
@@ -26,4 +26,25 @@ def report():
 
     else:
         return redirect("/")
-    return render_template("report.html", serchingBy=word, resultsNumber=len(jobs), jobs=jobs)
+    return render_template(
+        "report.html", searchingBy=word, resultsNumber=len(jobs), jobs=jobs
+    )
+
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get("word")
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv")
+    except:
+        return redirect("/")
+
+
+app.run(host="127.0.0.1")
